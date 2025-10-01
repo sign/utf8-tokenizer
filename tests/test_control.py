c04b50bb050a08eca38ec36da7d652a2c72baa7b
AmitMY <amitmoryossef@gmail.com>
2025-10-01 13:21:32 +0200

    feat(control): add control tokens visualization function

---
+++ tests/test_control.py
@@ -0,0 +1,41 @@
+import pytest
+
+from utf8_tokenizer.control import ControlTokens, visualize_control_tokens
+
+message = f"""
+{ControlTokens.StartOfText}{ControlTokens.StartOfHeading}system
+{ControlTokens.ShiftOut}You are a helpful assistant{ControlTokens.ShiftIn}{ControlTokens.EndOfTransmissionBlock}
+{ControlTokens.StartOfHeading}user
+{ControlTokens.ShiftOut}How much is 1+2?{ControlTokens.ShiftIn}{ControlTokens.EndOfTransmissionBlock}
+{ControlTokens.StartOfHeading}assistant
+First I'll think about it.
+{ControlTokens.Enquiry} The user wants me to calculate, I should call the calculator
+{ControlTokens.Substitute}{{``type'': ``calculator'', ``expression'': ``1+2''}}
+{ControlTokens.Escape}3{ControlTokens.Acknowledge}
+1 + 2 = 3{ControlTokens.EndOfTransmissionBlock}{ControlTokens.EndOfText}
+{ControlTokens.Null}{ControlTokens.Null}{ControlTokens.Null}{ControlTokens.Null}
+""".strip()
+
+visualized_message = """
+␂␁system
+␎You are a helpful assistant␏␗
+␁user
+␎How much is 1+2?␏␗
+␁assistant
+First I'll think about it.
+␅ The user wants me to calculate, I should call the calculator
+␚{``type'': ``calculator'', ``expression'': ``1+2''}
+␛3␆
+1 + 2 = 3␗␃
+␀␀␀␀
+""".strip()
+
+def test_control_visualization():
+    result = visualize_control_tokens(message)
+    print("result", result)
+
+    assert result == visualized_message
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v"])
--- utf8_tokenizer/control.py
+++ utf8_tokenizer/control.py
@@ -1,3 +1,6 @@
+import re
+
+
 class ControlTokens:
     Null = "\x00"
     StartOfHeading = "\x01"
@@ -36,3 +39,30 @@ class ControlTokens:
 
 
 CONTROl_TOKENS_PATTERN = "\x01-\x08\x0E-\x1F\x7F"
+
+CONTROL_WHITESPACES = {
+    ControlTokens.HorizontalTab,
+    ControlTokens.LineFeed,
+    ControlTokens.VerticalTab,
+    ControlTokens.FormFeed,
+    ControlTokens.CarriageReturn,
+    ControlTokens.Space,
+}
+
+
+def visualize_control_tokens(text: str, include_whitespace=False) -> str:
+    # Special visual handling for control characters using Control Pictures Unicode block
+    # Based on https://unicode.org/charts/nameslist/n_2400.html
+    def control_char_to_symbol(match):
+        char = match.group(0)
+        code = ord(char)
+        if not include_whitespace and char in CONTROL_WHITESPACES:
+            return char
+
+        if code <= 0x1F:  # Control characters 0x00-0x1F map to 0x2400-0x241F
+            return chr(0x2400 + code)
+        elif code == 0x7F:  # DELETE character maps to 0x2421
+            return chr(0x2421)
+        return char
+
+    return re.sub(r'[\x00-\x1F\x7F]', control_char_to_symbol, text)
